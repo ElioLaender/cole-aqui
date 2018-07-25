cd516e3147a05118d44386ed7cbd6f231135fee2
Laender <laenderquadros@gmail.com>
2018-07-24 23:19:46 -0300

    Atualizaçao

--- controller/ClientController.php
+++ controller/ClientController.php
@@ -5,15 +5,23 @@ include_once 'config/AutoLoadConfig.php';
 
 class ClientController extends ControllerConfig
 {
-    private $route;
+    private $route,
+            $aut,
+            $objCli,
+            $objUser;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->route = RouteConfig::rotas();
+        $this->aut = AutenticadorConfig::instanciar();
+        $this->objCli = new ClientDAO();
+        $this->objUser = new TradeUsersDAO();
+    }
 
     public function viewClientLogin()
     {
-        $this->route = RouteConfig::rotas();
-        $aut = AutenticadorConfig::instanciar();
-        $this->sessao = SessaoConfig::instanciar();
-
-        if ($aut->esta_logado() || $aut->login_cookie())
+        if ($this->aut->esta_logado() || $this->aut->login_cookie())
          {
             header('location: ?controller=Client&action=clientView');
         } else 
@@ -27,11 +35,7 @@ class ClientController extends ControllerConfig
 
     public function clientView()
     {
-        $this->route = RouteConfig::rotas();
-        $aut = AutenticadorConfig::instanciar();
-        $this->sessao = SessaoConfig::instanciar();
-
-        if ($aut->esta_logado() || $aut->login_cookie()) 
+        if ($this->aut->esta_logado() || $this->aut->login_cookie()) 
         {
             $this->view->set('URL_INI', $this->route['URL_INI']);
             $this->view->render($this->route['CLIENT_TRADE_INTERFACE']);
@@ -43,20 +47,15 @@ class ClientController extends ControllerConfig
 
     public function loginClient()
     {
-        $aut = AutenticadorConfig::instanciar();
-        $this->sessao = SessaoConfig::instanciar();
-
-        if ($aut->login(RequestConfig::getRequest('email'),md5(RequestConfig::getRequest('senha')))) 
+        if ($this->aut->login(RequestConfig::getRequest('email'),md5(RequestConfig::getRequest('senha')))) 
         {
             #verifica se o usuário deseja se manter logado
             if(RequestConfig::getPost('manterConectado') == "logado")
             {
                 #caso passar pelo método login, é porque os dados batem com o que está no servidor. Nesse caso será armazenado a hash criptografada no cookie.
-                $aut->setPermanecerLogado(RequestConfig::getPost('email'), md5(RequestConfig::getPost('senha')));//Resolver problema de não poder salvar senha com criptografia no cookie.
-            }
-		
- 	            $aut = AutenticadorConfig::instanciar();
-                echo $aut->userRef();
+                $this->aut->setPermanecerLogado(RequestConfig::getPost('email'), md5(RequestConfig::getPost('senha')));//Resolver problema de não poder salvar senha com criptografia no cookie.
+            }  
+                echo $this->aut->userRef();
         } else 
         {
             echo "0";
@@ -65,17 +64,13 @@ class ClientController extends ControllerConfig
 
     public function returnCli()
     {
-        $objCli = new ClientDAO();
-        $aut = AutenticadorConfig::instanciar();
-        $objUser = new TradeUsersDAO();
-
         if(isset($_GET['ref']) && !empty($_GET['ref']))
         {
-        $objCli->ClientAndTrade($_GET['cpf'],$_GET['ref']);
+        $this->objCli->ClientAndTrade($_GET['cpf'],$_GET['ref']);
         } else 
         {
-        $data = $objUser->tradeFromUser($aut->userRef(),'no');
-        $objCli->ClientAndTrade($_GET['cpf'],$data[0]['trade_id']);
+        $data = $this->objUser->tradeFromUser($this->aut->userRef(),'no');
+        $this->objCli->ClientAndTrade($_GET['cpf'],$data[0]['trade_id']);
         }
     }
 }
--- controller/UploadController.php
+++ controller/UploadController.php
@@ -29,5 +29,5 @@ class UploadController extends ControllerConfig
           return  0;
            
         }
-    }
+    }-
 }
